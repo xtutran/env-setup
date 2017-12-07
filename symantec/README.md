c5a45d0f801f1018e9df6fcde36122cd6561f339
xtutran <tran.xuantu@axa.com>
2017-12-07 12:14:17 +0800

    start/stop Symantec

---
+++ symantec/README.md
@@ -0,0 +1,12 @@
+## Start/Stop Symantec Antivirus [ref](https://gist.github.com/steve-jansen/61a189b6ab961a517f68)
+
+#### Start the service
+```bash
+./run.sh -m load
+``` 
+
+#### Stop the service
+```bash
+./run.sh -m unload
+```
+
---
+++ symantec/run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+while getopts ":hm:" opt; do
+  case ${opt} in
+    m )
+      cmd=$OPTARG
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument [load|unload]" 1>&2
+      ;;
+    h ) echo "Usage: ./run.sh -m [load|unload]"
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+if [[ "$cmd" == "load" || "$cmd" == "unload" ]]; then
+    sudo launchctl $cmd /Library/LaunchDaemons/com.symantec.symdaemon.plist
+    sudo launchctl $cmd /Library/LaunchDaemons/com.symantec.sharedsettings.plist
+    launchctl $cmd /Library/LaunchAgents/com.symantec.uiagent.application.plist
+else
+    echo "Invalid option: $cmd requires an argument [load|unload]" 1>&2
+fi
+
